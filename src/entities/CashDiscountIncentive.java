198853acdce4d00a50229dd47567bf890faeb9b7
Soumya Nayar <nayar.s@northeastern.edu>
2021-04-15 07:56:09 -0700

    Update Rebate Incentive Entity

--- src/entities/CashDiscountIncentive.java
+++ src/entities/CashDiscountIncentive.java
@@ -1,7 +1,6 @@
 package entities;
 
 import java.util.Date;
-import java.util.List;
 
 public class CashDiscountIncentive extends Incentive {
     private double value;
--- src/entities/RebateIncentive.java
+++ src/entities/RebateIncentive.java
@@ -1,33 +1,23 @@
 package entities;
 
 import java.util.Date;
-import java.util.List;
+import java.util.HashMap;
 
 public class RebateIncentive extends Incentive{
-    private String type;
-    private double amount;
+    public HashMap<String, Double> getRebateMap() {
+        return rebateMap;
+    }
+
+    public void setRebateMap(HashMap<String, Double> rebateMap) {
+        this.rebateMap = rebateMap;
+    }
+
+    private HashMap<String, Double> rebateMap;
 
     public RebateIncentive(String id, IncentiveType incentiveType, String dealerId, Date startDate, Date endDate, String title, String description,
                            String disclaimer, CarCategory carCategory, String make,
-                           String model, String startYear, String endYear, String thresholdMiles, String type, double amount) {
+                           String model, String startYear, String endYear, String thresholdMiles, HashMap<String, Double> rebateMap) {
         super(id, incentiveType, dealerId, startDate, endDate, title, description, disclaimer, carCategory, make, model, startYear, endYear, thresholdMiles);
-        this.type = type;
-        this.amount = amount;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
+        this.rebateMap = rebateMap;
     }
 }
