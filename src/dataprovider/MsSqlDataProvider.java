d10d4419b86067ce5368ece6b4ab3cccee0046e6
Soumya Nayar <nayar.s@northeastern.edu>
2021-04-20 02:33:58 -0700

    MsSql Data Provider Implementation, Partial

--- src/dataprovider/MsSqlDataProvider.java
+++ src/dataprovider/MsSqlDataProvider.java
@@ -1,12 +1,10 @@
 package dataprovider;
 
-import entities.CashDiscountIncentive;
-import entities.CashDiscountType;
+import entities.*;
+
 import java.sql.*;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.UUID;
+import java.sql.Date;
+import java.util.*;
 
 public class MsSqlDataProvider {
     private static final String URL
@@ -18,11 +16,19 @@ public class MsSqlDataProvider {
     private static MsSqlDataProvider _instance;
     private Statement dbStatement;
 
+    // Singleton Pattern
     static public MsSqlDataProvider getInstance() throws SQLException, ClassNotFoundException {
         if (_instance == null) {
             _instance = new MsSqlDataProvider();
+            _instance.createRequiredTablesIfDoesNotExists();
         }
         return _instance;
+    }
+
+    private void createRequiredTablesIfDoesNotExists() throws SQLException {
+        this.createIncentiveTableIfDoesnotExisit();
+        this.createIncentiveVINsTableIfDoesnotExisit();
+        this.createIncentiveRebatesTableIfDoesnotExisit();
     }
 
     private MsSqlDataProvider() throws SQLException, ClassNotFoundException {
@@ -31,7 +37,7 @@ public class MsSqlDataProvider {
         this.dbStatement = dbConnection.createStatement();
     }
 
-    private void createIncentiveTable() throws SQLException {
+    private void createIncentiveTableIfDoesnotExisit() throws SQLException {
         this.dbStatement.execute("if not exists (select * from sysobjects where name='Incentive' and xtype='U')" +
                 "CREATE TABLE dbo.Incentive (" +
                 "id VARCHAR(40) PRIMARY KEY NOT NULL," +
@@ -53,14 +59,14 @@ public class MsSqlDataProvider {
                 "rebateMapUUID VARCHAR(40) DEFAULT(NULL))");
     }
 
-    private void createIncentiveVINsTable() throws SQLException {
+    private void createIncentiveVINsTableIfDoesnotExisit() throws SQLException {
         this.dbStatement.execute("if not exists (select * from sysobjects where name='IncentiveVINs' and xtype='U')" +
                 "CREATE TABLE dbo.IncentiveVINs (" +
                 "incentiveVinID VARCHAR(40) NOT NULL," +
                 "carVIN VARCHAR(40) NOT NULL)");
     }
 
-    private void createIncentiveRebatesTable() throws SQLException {
+    private void createIncentiveRebatesTableIfDoesnotExisit() throws SQLException {
         this.dbStatement.execute("if not exists (select * from sysobjects where name='IncentiveRebates' and xtype='U')" +
                 "CREATE TABLE dbo.IncentiveRebates (" +
                 "rebateID VARCHAR(40) NOT NULL," +
@@ -68,7 +74,7 @@ public class MsSqlDataProvider {
                 "rebateValue DECIMAL NOT NULL)");
     }
 
-    private void persistIncentive(CashDiscountIncentive cashDiscountIncentive) throws SQLException {
+    public void persistIncentive(CashDiscountIncentive cashDiscountIncentive) throws SQLException {
         String carVinUUID = UUID.randomUUID().toString();
         String sql = "INSERT INTO Incentive(id, incentiveType, dealerId, startDate, endDate, title, description, disclaimer, carVinUUID, " +
                 "cashDiscountType, discountValue) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
@@ -95,16 +101,104 @@ public class MsSqlDataProvider {
         }
     }
 
+    public void persistIncentive(LoanIncentive loanDiscountIncentive) throws SQLException {
+        String carVinUUID = UUID.randomUUID().toString();
+        String sql = "INSERT INTO Incentive(id, incentiveType, dealerId, startDate, endDate, title, description, disclaimer, carVinUUID, " +
+                "apr, loanmonths) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+        PreparedStatement preparedStatement = this.dbConnection.prepareStatement(sql);
+        preparedStatement.setString(1, loanDiscountIncentive.getId());
+        preparedStatement.setString(2, loanDiscountIncentive.getIncentiveType().toString());
+        preparedStatement.setString(3, loanDiscountIncentive.getDealerId());
+        preparedStatement.setDate(4, new java.sql.Date(loanDiscountIncentive.getStartDate().getTime()));
+        preparedStatement.setDate(5, new java.sql.Date(loanDiscountIncentive.getEndDate().getTime()));
+        preparedStatement.setString(6, loanDiscountIncentive.getTitle());
+        preparedStatement.setString(7, loanDiscountIncentive.getDescription());
+        preparedStatement.setString(8, loanDiscountIncentive.getDisclaimer());
+        preparedStatement.setString(9, carVinUUID);
+        preparedStatement.setDouble(10, loanDiscountIncentive.getApr());
+        preparedStatement.setInt(11, loanDiscountIncentive.getMonths());
+        preparedStatement.execute();
+
+        sql = "INSERT INTO IncentiveVINs(incentiveVinID, carVIN) VALUES (?, ?)";
+        preparedStatement = this.dbConnection.prepareStatement(sql);
+        for (String carVIN : loanDiscountIncentive.getCarVINList()) {
+            preparedStatement.setString(1, carVinUUID);
+            preparedStatement.setString(2, carVIN);
+            preparedStatement.execute();
+        }
+    }
+
+    public void persistIncentive(LeasingIncentive leasingIncentive) throws SQLException {
+        String carVinUUID = UUID.randomUUID().toString();
+        String sql = "INSERT INTO Incentive(id, incentiveType, dealerId, startDate, endDate, title, description, disclaimer, carVinUUID, " +
+                "leasemonths, signingPay, monthlyPay) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+        PreparedStatement preparedStatement = this.dbConnection.prepareStatement(sql);
+        preparedStatement.setString(1, leasingIncentive.getId());
+        preparedStatement.setString(2, leasingIncentive.getIncentiveType().toString());
+        preparedStatement.setString(3, leasingIncentive.getDealerId());
+        preparedStatement.setDate(4, new java.sql.Date(leasingIncentive.getStartDate().getTime()));
+        preparedStatement.setDate(5, new java.sql.Date(leasingIncentive.getEndDate().getTime()));
+        preparedStatement.setString(6, leasingIncentive.getTitle());
+        preparedStatement.setString(7, leasingIncentive.getDescription());
+        preparedStatement.setString(8, leasingIncentive.getDisclaimer());
+        preparedStatement.setString(9, carVinUUID);
+        preparedStatement.setInt(10, leasingIncentive.getMonths());
+        preparedStatement.setDouble(11, leasingIncentive.getSigningPay());
+        preparedStatement.setDouble(12, leasingIncentive.getMonthlyPay());
+        preparedStatement.execute();
+
+        sql = "INSERT INTO IncentiveVINs(incentiveVinID, carVIN) VALUES (?, ?)";
+        preparedStatement = this.dbConnection.prepareStatement(sql);
+        for (String carVIN : leasingIncentive.getCarVINList()) {
+            preparedStatement.setString(1, carVinUUID);
+            preparedStatement.setString(2, carVIN);
+            preparedStatement.execute();
+        }
+    }
+
+    public void persistIncentive(RebateIncentive rebateIncentive) throws SQLException {
+        String carVinUUID = UUID.randomUUID().toString();
+        String rebateMapUUID = UUID.randomUUID().toString();
+        String sql = "INSERT INTO Incentive(id, incentiveType, dealerId, startDate, endDate, title, description, disclaimer, carVinUUID, " +
+                "rebateMapUUID) VALUES (?,?,?,?,?,?,?,?,?,?)";
+        PreparedStatement preparedStatement = this.dbConnection.prepareStatement(sql);
+        preparedStatement.setString(1, rebateIncentive.getId());
+        preparedStatement.setString(2, rebateIncentive.getIncentiveType().toString());
+        preparedStatement.setString(3, rebateIncentive.getDealerId());
+        preparedStatement.setDate(4, new java.sql.Date(rebateIncentive.getStartDate().getTime()));
+        preparedStatement.setDate(5, new java.sql.Date(rebateIncentive.getEndDate().getTime()));
+        preparedStatement.setString(6, rebateIncentive.getTitle());
+        preparedStatement.setString(7, rebateIncentive.getDescription());
+        preparedStatement.setString(8, rebateIncentive.getDisclaimer());
+        preparedStatement.setString(9, carVinUUID);
+        preparedStatement.setString(10, rebateMapUUID);
+        preparedStatement.execute();
+
+        sql = "INSERT INTO IncentiveVINs(incentiveVinID, carVIN) VALUES (?, ?)";
+        preparedStatement = this.dbConnection.prepareStatement(sql);
+        for (String carVIN : rebateIncentive.getCarVINList()) {
+            preparedStatement.setString(1, carVinUUID);
+            preparedStatement.setString(2, carVIN);
+            preparedStatement.execute();
+        }
+
+        sql = "INSERT INTO IncentiveRebates(rebateID, rebateType, rebateValue) VALUES (?, ?, ?)";
+        preparedStatement = this.dbConnection.prepareStatement(sql);
+        for (Map.Entry<String, Double> mapElement : rebateIncentive.getRebateMap().entrySet()) {
+            preparedStatement.setString(1, rebateMapUUID);
+            preparedStatement.setString(2, mapElement.getKey());
+            preparedStatement.setDouble(3, mapElement.getValue());
+            preparedStatement.execute();
+        }
+    }
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         MsSqlDataProvider msSqlDataProvider = MsSqlDataProvider.getInstance();
-        msSqlDataProvider.createIncentiveTable();
-        msSqlDataProvider.createIncentiveVINsTable();
-        msSqlDataProvider.createIncentiveRebatesTable();
         CashDiscountIncentive cashDiscountIncentive = new CashDiscountIncentive(
                 UUID.randomUUID().toString(),
                 UUID.randomUUID().toString(),
-                new Date(),
-                new Date(),
+                new Date(System.currentTimeMillis()),
+                new Date(System.currentTimeMillis()),
                 "title1",
                 "description1",
                 "disclaimer1",
@@ -112,6 +206,53 @@ public class MsSqlDataProvider {
                 1000,
                 CashDiscountType.FLATAMOUNT
         );
+
+        LoanIncentive loanIncentive = new LoanIncentive(
+                UUID.randomUUID().toString(),
+                UUID.randomUUID().toString(),
+                new Date(System.currentTimeMillis()),
+                new Date(System.currentTimeMillis()),
+                "title1",
+                "description1",
+                "disclaimer1",
+                new HashSet<>(Arrays.asList(UUID.randomUUID().toString(), UUID.randomUUID().toString(), UUID.randomUUID().toString(), UUID.randomUUID().toString())),
+                3.4,
+                72
+        );
+
+        LeasingIncentive leasingIncentive = new LeasingIncentive(
+                UUID.randomUUID().toString(),
+                UUID.randomUUID().toString(),
+                new Date(System.currentTimeMillis()),
+                new Date(System.currentTimeMillis()),
+                "title1",
+                "description1",
+                "disclaimer1",
+                new HashSet<>(Arrays.asList(UUID.randomUUID().toString(), UUID.randomUUID().toString(), UUID.randomUUID().toString(), UUID.randomUUID().toString())),
+                36,
+                4000.0,
+                500.0
+        );
+
+        RebateIncentive rebateIncentive = new RebateIncentive(
+                UUID.randomUUID().toString(),
+                UUID.randomUUID().toString(),
+                new Date(System.currentTimeMillis()),
+                new Date(System.currentTimeMillis()),
+                "title1",
+                "description1",
+                "disclaimer1",
+                new HashSet<>(Arrays.asList(UUID.randomUUID().toString(), UUID.randomUUID().toString(), UUID.randomUUID().toString(), UUID.randomUUID().toString())),
+                new HashMap<String, Double>() {
+                    {
+                        put("NewGradRebate", 2000.0);
+                        put("MilitaryVeteranRebate", 3000.0);
+                    }
+                }
+                );
         msSqlDataProvider.persistIncentive(cashDiscountIncentive);
+        msSqlDataProvider.persistIncentive(loanIncentive);
+        msSqlDataProvider.persistIncentive(leasingIncentive);
+        msSqlDataProvider.persistIncentive(rebateIncentive);
     }
 }
